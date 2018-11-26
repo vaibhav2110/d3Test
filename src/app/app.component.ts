6baa99966081e15bc537e5de8a58061943054470
Vaibhav <vaibhpraky@gmail.com>
2018-11-26 18:00:46 +0530

    d3Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit{
   title = 'd3Test';
+  ngAfterContentInit() {
+      let height = 500, width = 1000,
+        margin = {
+            left: 120, right: 20, top: 20, bottom: 70
+        },
+        url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",
+        months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+        color = ["#ef5350","#EC407A","#AB47BC","#7E57C2","#5C6BC0","#42A5F5","#26C6DA","#26A69A","#D4E157","#FFEE58","#FFA726"];
+
+        let canvas = d3.select('svg').attr({
+          height: height + margin.top + margin.bottom,
+          width: width + margin.left + margin.right
+        });
+
+        let group = canvas.append('g').attr({
+          transform: "translate(" + margin.left + "," + margin.top + ")"
+        });
+
+        let div = d3.select('.tooltip');
+        let xScale = d3.time.scale().range([0,width]);
+        let yScale = d3.scale.ordinal().domain(months).rangeBands([0,height]);
+        let colorScale = d3.scale.quantize().range(color);
+
+
+//call the json with the url and a function
+        d3.json(url,(data)=>{
+          data = data.monthlyVariance;
+          //map the data
+          data.map(function(d){
+            d.month = months[d.month-1];
+            d.year = d3.time.format("%Y").parse(d.year.toString());
+          })
+
+          //declair the function xScale
+          xScale.domain(d3.extent(data,function(data){
+            return data.year;
+          }));
+          //scale the colors
+          colorScale.domain(d3.extent(data,function(d){
+            return d.variance;
+          }));
+
+          let barWidth = width / (data.length / 12)
+          let barHeight = height / 12;
+          //scale the axis and set orientation
+          let xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+          let yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(12);
+
+          //add a g element that provides a reference point for adding the axes.
+          //append the axis according to the size and the position that we have set up with our width, height and margin’s.
+          group.append('g').attr({
+            class: "xAxis",
+            transform: "translate(0," + (height) + ")"
+          }).call(xAxis);
+          group.append('g').attr({
+            class: "yAxis",
+            transform: "translate(0,0)",
+          }).call(yAxis);
+          group.selectAll('g').data(data).enter().append('g').attr({
+            transform: function(data){
+              return "translate(" + xScale(data.year) + "," + yScale(data.month) +  ")";
+            }
+            }).append('rect').attr({ //append the rectangulars and scale them in the graph
+            width: barWidth,
+            height: yScale.rangeBand()
+          }).style({ //scale the colors
+            fill: function(data){
+             return colorScale(data.variance);
+            }
+          }).on("mouseover",function(d){
+            //make the tooltip to follow the mouse
+            div.transition().duration(10).style("opacity",0.8).style({
+              left: d3.event.pageX + "px",
+              top: d3.event.pageY + "px"
+            });
+
+            //appear/disappear the tooltip on mouse hover
+            //8.66 is a standard value
+            div.html("<p>Year: " + d3.time.format("%Y")(d.year) + "</p></p>Value = " + (8.66 + d.variance).toFixed(2) + "</p><p>Month: " + d.month + "</p>" );
+          }).on("mouseout",function(d){
+            div.transition().duration(100).style("opacity",0);
+          });
+
+        });
+  }
 }
